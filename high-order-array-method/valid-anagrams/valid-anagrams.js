f3cdff2a43207a85481be59c1ecc195d391907a6
Erfan Pournasiri <erfanpournasiri71@gmail.com>
2024-05-04 16:29:12 +0330

    update valid anagrams solution

--- high-order-array-method/valid-anagrams/valid-anagrams.js
+++ high-order-array-method/valid-anagrams/valid-anagrams.js
@@ -1,19 +1,34 @@
 function ValidAnagrams(str1,str2){
-    const freqCount1 = str1.split('').reduce((acc,char)=>{
-        acc[char] = (acc[char] || 0) + 1
-        return acc
-    },{})
-    const freqCount2 = str2.split('').reduce((acc,char)=>{
-        acc[char] = (acc[char] || 0) + 1
-        return acc
-    },{})
+    // const freqCount1 = str1.split('').reduce((acc,char)=>{
+    //     acc[char] = (acc[char] || 0) + 1
+    //     return acc
+    // },{})
+    // const freqCount2 = str2.split('').reduce((acc,char)=>{
+    //     acc[char] = (acc[char] || 0) + 1
+    //     return acc
+    // },{})
 
 
-    return Object.keys(freqCount1).every((char)=>
-        freqCount1[char] === freqCount2[char]
-    )
+    // return Object.keys(freqCount1).every((char)=>
+    //     freqCount1[char] === freqCount2[char]
+    // )
+
+
+    // solution 2
+    if(str1.length !== str2.length){
+        return false
+    }
+    const charArray1 = str1.split('').sort()
+    const charArray2 = str2.split('').sort()
+    for(let i = 0; i < charArray1.length; i++){
+        if(charArray1[i] !== charArray2[i]){
+            return false
+        }
+    }
+    return true
 
 }
+
 
 
 module.exports = ValidAnagrams
